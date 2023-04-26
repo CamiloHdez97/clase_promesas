b11d24851ae99ad249c4a93902d6d1152abd6dce
CamiloHdez97 <45055602+CamiloHdez97@users.noreply.github.com>
2023-04-26 08:29:46 -0500

    Agregado correo
    Agregado correo desde GItHub

--- promesa1.js
+++ promesa1.js
@@ -1,10 +1,12 @@
 const getUser = () => {
 	return {
 		uid: 'ABC123',
-		username: 'El_Papi1502'
+		username: 'El_Papi1502',
+		corre: 'correo@correo'
 	}
 }
 
 console.log(getUser());
 const user = getUser();
 console.log(user);
+
